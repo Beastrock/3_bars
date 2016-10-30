050c0a21cd1977ec083e4f5594deb9cf971d92a4
Alexey <beastrock@mail.ru>
2016-10-30 21:32:31 +0300

    Fixed bugs
    Fixed miscalculated bugs. Changed the formula of distance.

--- bars.py
+++ bars.py
@@ -1,7 +1,7 @@
 import json
 import os.path
 import sys
-from math import asin, acos, atan2, radians, sqrt
+from math import sin, cos, atan2, radians, sqrt, fabs
 
 
 def load_data(filepath):
@@ -23,47 +23,49 @@ def get_biggest_bar(bars):
 
 def calculate_closest_bar(bars, user_latitude, user_longitude):
     def calculate_distance(bars):
-        EARTH_RADIUS = 6371e3
-        bar_coordinates = bars["Cells"]["geoData"].get('coordinates')
-        bar_latitude = bar_coordinates[0]
-        bar_longitude = bar_coordinates[1]
-        fi1 = radians(user_longitude)
-        fi2 = radians(bar_longitude)
-        delta_fi = radians(bar_latitude - user_latitude)
-        delta_lambda = radians(bar_longitude - user_longitude)
-        # calculate a - the square of half the chord length between the points.
-        a = (asin(delta_fi / 2) ** 2) * asin(delta_fi / 2) \
-            + acos(fi1) * acos(fi2) * (asin(delta_lambda / 2) ** 2)
-        # calculate c - the angular distance in radians
-        c = 2 * atan2(sqrt(a), sqrt(1 - a))
-        # calculate d - the desired length between bar and user
-        d = int(EARTH_RADIUS * c)
-        return d
+        bar_longitude = bars["Cells"]["geoData"]["coordinates"][0]
+        bar_latitude = bars["Cells"]["geoData"]["coordinates"][1]
+        # sin and cos of latitudes
+        cosl1 = cos(radians(user_latitude))
+        cosl2 = cos(radians(bar_latitude))
+        sinl1 = sin(radians(user_latitude))
+        sinl2 = sin(radians(bar_latitude))
+        # sin and cos of longitude difference
+        longitude_delta = radians(bar_longitude - user_longitude)
+        cos_delta = cos(longitude_delta)
+        sin_delta = sin(longitude_delta)
+        # haversine formula
+        numerator = sqrt(pow(cosl2 * sin_delta, 2) +
+                         pow(cosl1 * sinl2 - sinl1 * cosl2 * cos_delta, 2))
+        denominator = sinl1 * sinl2 + cosl1 * cosl2 * cos_delta
+        # result of haversine formula(angle)
+        angular_disparity = atan2(numerator, denominator)
+        earth_radius = 6372795  # in metres
+        distance = angular_disparity * earth_radius
+        return round(distance)
 
     nearest_bar = min(bars, key=calculate_distance)
-    distance = {"Distance": calculate_distance(nearest_bar) / 1000}
-    nearest_bar.update(distance)
+    nearest_bar.update({"Distance": calculate_distance(nearest_bar)})
     return nearest_bar
 
 
 def print_results(smallest_bar, biggest_bar, nearest_bar):
-    print("The smallest bar \"{b[Name]}\" +"
-          "There are {b[SeatsCount]} seats.".format(b=smallest_bar["Cells"]))
-    print("The biggest bar \"{b[Name]}\"."
-          " There are {b[SeatsCount]} seats.".format(b=biggest_bar["Cells"]))
-    print("The nearest bar, \"{}\", is by {} km. "
-          "Address to find it: {}".format(nearest_bar["Cells"]["Name"],
-                                          nearest_bar["Distance"],
-                                          nearest_bar["Cells"]["Address"]))
+    print("The smallest bar \"{b[Name]}\":"
+          " {b[SeatsCount]} seats.".format(b=smallest_bar["Cells"]))
+    print("The biggest bar \"{b[Name]}\:"
+          " {b[SeatsCount]} seats.".format(b=biggest_bar["Cells"]))
+    print("The nearest bar \"{}\" is in {} metres."
+          " Address to find it: {}".format(nearest_bar["Cells"]["Name"],
+                                           nearest_bar["Distance"],
+                                           nearest_bar["Cells"]["Address"]))
 
 
 if __name__ == '__main__':
     json_filepath = "bars.json"
     bars = load_data(json_filepath)
-    user_longitude, user_latitude = input().split()
     the_biggest = get_biggest_bar(bars)
     the_smallest = get_smallest_bar(bars)
-    user_longitude = 55.755797
-    user_latitude = 37.408770
+    user_latitude = float(input("Input your latitude in degrees:\n"))
+    user_longitude = float(input("Input your longitude in degrees:\n"))
     nearest_bar = calculate_closest_bar(bars, user_latitude, user_longitude)
     print_results(the_smallest, the_biggest, nearest_bar)
